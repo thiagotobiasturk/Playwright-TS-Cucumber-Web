617c56aebc1a00e9b282e64fbdfea425d0055d4f
aduartemp <aduarte@temperies>
2024-08-26 01:44:56 -0300

    push

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,5 +1,5 @@
 import { BeforeAll, AfterAll, Before, After, AfterStep, Status } from "@cucumber/cucumber";
-import { chromium, Browser, Page, expect, BrowserContext } from "@playwright/test";
+import { chromium, Browser, Page, BrowserContext } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
 
 let browser: Browser;
@@ -15,17 +15,14 @@ Before(async function () {
     pageFixture.page = page;
 });
 
-AfterStep(async function ({ pickle, result }) {
-    const img = await pageFixture.page.screenshot({ path: `./test-result/screenshots/${pickle.name}.png`, type: "png" });
-    await this.attach(img, "image/png");
-});
-
-After(async function ({ pickle, result }) {
-    if (result?.status === Status.FAILED) {
-        const img = await pageFixture.page.screenshot({ path: `./test-result/screenshots/${pickle.name}.png`, type: "png" });
+AfterStep(async function (step) {
+    if (step.result?.status === Status.FAILED) {
+        const img = await pageFixture.page.screenshot({ type: "png" });
         await this.attach(img, "image/png");
     }
+});
 
+After(async function () {
     await pageFixture.page.close();
     const newPage = await context.newPage();
     pageFixture.page = newPage;
